6d4700ef9a40cbdcf4eee1a9dba3f8b6d5fd0845
petermirithu <pyra_m.k@yahoo.com>
2019-11-22 23:05:05 +0300

    import and pass the model classes in shell

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.db_class import User,Role,Pitch,Comment
 
 app=create_app('development')
 
@@ -8,7 +9,7 @@ manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-  return dict(app=app)
+  return dict(app=app,db=db,Role=Role,Pitch=Pitch,Comment=Comment)
 
 if __name__=='__main__':
   manager.run()
