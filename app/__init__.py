46d7b9340def5842d3a94d4ae6a158302bc97ca6
petermirithu <pyra_m.k@yahoo.com>
2019-11-22 21:01:44 +0300

    import config_options from config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import config_options
 from flask_bootstrap import Bootstrap
 
 
@@ -8,6 +9,8 @@ bootstrap=Bootstrap()
 def create_app(config_name):
 
   app=Flask(__name__)
+
+  app.config.from_object(config_options[config_name])
 
   from .lead import lead as lead_blueprint
   app.register_blueprint(lead_blueprint)
