096c9d87d87162a6e9256fc208c9d03f46e6231a
petermirithu <pyra_m.k@yahoo.com>
2019-11-24 15:30:13 +0300

    add comment feature to each pitch

--- app/lead/views.py
+++ app/lead/views.py
@@ -1,9 +1,11 @@
 from flask import render_template,redirect,request,url_for,abort
 from . import lead
 from flask_login import login_required,current_user
-from ..db_class import User,Pitch
-from .forms import UpdateProfile,PitchForm
+from ..db_class import User,Pitch,Comment
+from .forms import UpdateProfile,PitchForm,CommentForm
 from .. import db,photos
+import markdown2
+
 
 
 @lead.route('/')
@@ -18,12 +20,25 @@ def index():
 @lead.route('/pitch/<category>')
 def pitch(category):
   '''
-  view function that renders pich template with pitches for that category
+  view function that renders comment template with pitches for that category
   '''
   title=category
   pitches=Pitch.get_pitches(category)
   
   return render_template('pitch.html',title=title,pitches=pitches)
+
+@lead.route('/pitch/comment/<id>')
+def comment(id):
+  '''
+  view function that renders comment template with comments for that pitch
+  '''
+  pitch=Pitch.query.filter_by(id=id).first()
+  pitch_id=pitch.id
+  
+  title='Comment'
+  # comments=Comment.get_comments(id)
+    
+  return render_template('comment.html',title=title,pitch_id=pitch_id)
 
 @lead.route('/user/<name>')  
 def profile(name):
@@ -78,20 +93,42 @@ def update_pic(name):
 @lead.route('/pitch/new/<category>', methods = ['GET','POST'])
 @login_required
 def new_pitch(category):
+  '''
+  view function that posts a new pitch to a specific category
+  '''
   form=PitchForm()
 
   category=category
   if form.validate_on_submit():
-    p_title=form.p_title.data
+    p_title=form.p_title.data    
     pitch_it=form.pitch_it.data
+    pitch_format=markdown2.markdown(pitch_it,extras=["code-friendly", "fenced-code-blocks"])
     username=current_user.username
-
-    print(f'**************************{username}******************')
-    new_pitch=Pitch(category=category,p_title=p_title,pitch_it=pitch_it,poster_name=username)
+    
+    new_pitch=Pitch(category=category,p_title=p_title,pitch_it=pitch_format,posted_by=username)
     
     new_pitch.save_pitch()
     return redirect(url_for('.pitch',category = category))
 
-  title="{category}"  
+  title=category
   return render_template('new_pitch.html',title=title,PitchForm=form)
 
+@lead.route('/pitch/comment/new/<id>')
+@login_required
+def new_comment(id):
+  '''
+  view function that registers new comments for a pitch
+  '''  
+  form=CommentForm()
+
+  if form.validate_on_submit():    
+    p_comment=form.p_comment.data
+
+    new_comment=Comment(pitch_id=id,p_comment=p_comment,comment_by=current_user.username)
+
+    new_comment.save_comment()
+
+    return redirect(url_for('lead.index',id=id))
+
+  title='Comment'
+  return render_template('new_comment.html',title=title,CommentForm=form)  
