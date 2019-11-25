437dc605c6c45276971a6a485d2f5e98ab36bf7b
Pyra <55141332+petermirithu@users.noreply.github.com>
2019-11-25 22:43:27 +0300

    delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-export SECRET_KEY='\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
-export MAIL_USERNAME='pyra008.m.k@gmail.com'
-export MAIL_PASSWORD='Lotus007'
-
-python3 manage.py server
